4a6d16bfd881fede62e21dfff275a729021cde77
Kesishere <kesishere@gmail.com>
2018-12-11 01:01:05 +0300

    v2.1

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -17,31 +17,33 @@ class NavBar extends Component {
         return (
             <div>
                 <Navbar>
-                    <Nav>
-                        <NavItem eventKey={1} href="/">
-                            Главная
+                    <Navbar.Header>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem eventKey={1} href="/">
+                                Главная
                          </NavItem>
-                        <NavDropdown eventKey={2} title="Услуги" id="basic-nav-dropdown">
-                            <MenuItem eventKey={2.1} href="/terapy">Терапия</MenuItem>
-                            <MenuItem eventKey={2.2} href="/endodont">Эндодонтия</MenuItem>
-                            <MenuItem eventKey={2.3} href="/surgery">Хирургия</MenuItem>
-                            <MenuItem eventKey={2.4} href="/orthodontics">Ортодонтия</MenuItem>
-                            <MenuItem eventKey={2.5} href="/hygiene">Гигиена</MenuItem>
-                            <MenuItem eventKey={2.6} href="/orthopedics">Ортопедия</MenuItem>
-                        </NavDropdown>
-                        <NavDropdown eventKey={3} title="Для клиентов" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1} href="/docs">Нормативные документы</MenuItem>
-                            <MenuItem eventKey={3.2} href="/politics">Политика конфедициальности</MenuItem>
-                            <MenuItem eventKey={3.3} href="/req">Реквизиты</MenuItem>
-                            <MenuItem divider />
-                            <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                        </NavDropdown>
-                        <NavItem eventKey={4} href="/specs">
-                            Специалисты
+                            <NavDropdown eventKey={2} title="Услуги" id="basic-nav-dropdown">
+                                <MenuItem eventKey={2.1} href="/terapy">Терапия</MenuItem>
+                                <MenuItem eventKey={2.2} href="/endodont">Эндодонтия</MenuItem>
+                                <MenuItem eventKey={2.3} href="/surgery">Хирургия</MenuItem>
+                                <MenuItem eventKey={2.4} href="/orthodontics">Ортодонтия</MenuItem>
+                                <MenuItem eventKey={2.5} href="/hygiene">Гигиена</MenuItem>
+                                <MenuItem eventKey={2.6} href="/orthopedics">Ортопедия</MenuItem>
+                            </NavDropdown>
+                            <NavDropdown eventKey={3} title="Для клиентов" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1} href="/docs">Нормативные документы</MenuItem>
+                                <MenuItem eventKey={3.2} href="/politics">Политика конфедициальности</MenuItem>
+                                <MenuItem eventKey={3.3} href="/req">Реквизиты</MenuItem>
+                            </NavDropdown>
+                            <NavItem eventKey={4} href="/specs">
+                                Специалисты
                          </NavItem>
-                    </Nav>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
-
             </div>
         )
     }
